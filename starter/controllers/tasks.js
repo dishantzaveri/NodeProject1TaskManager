be6f25d11fc8ae712d90ff98296d07c2f7991dff
dishantzaveri <zaveridishant@gmail.com>
2022-08-10 03:53:01 +0530

    post req

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,16 +1,21 @@
+const Task = require('../models/task');
+
+
+
 const getAllTasks = (req,
     res) => {
     res.send('get all tasks');
 }
 
-const createTask = (req,
+const createTask = async (req,
     res) => {
-    res.send('create task');
+    const task = await Task.create(req.body);
+   res.status(201).json({task})
 }
 
 const getTask = (req,
     res) => {
-    res.send('get task');
+   res.json({id: req.params.id})
 }
 
 const updateTask = (req,
